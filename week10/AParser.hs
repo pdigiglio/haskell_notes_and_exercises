60f14ff23a8086d5df3e1b567d7d24c9fe8834de
Paolo Di Giglio <p.digiglio91@gmail.com>
2024-03-03 19:55:29 +0100

    week10: add my solutions

--- week10/AParser.hs
+++ week10/AParser.hs
@@ -57,3 +57,74 @@ posInt = Parser f
 ------------------------------------------------------------
 -- Your code goes below here
 ------------------------------------------------------------
+
+{- Exercise 1 -}
+
+-- | Apply a function to the first element of a pair.
+--
+-- This is effectively a @fmap@ for pairs operating on the first element
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (x, y) = (f x, y)
+
+instance Functor Parser where
+  fmap f pa = Parser $ fmap (first f) . runParser pa
+
+{- Exercise 2 -}
+
+instance Applicative Parser where
+  -- | Construct a parser that consumes no input and return its value as if it
+  -- had been parsed.
+  --
+  -- Alternative implementation, enabling @TupleSections@:
+  --
+  -- > pure x = Parser $ Just . (x,)
+  pure x = Parser (\s -> Just (x, s))
+
+  -- Parser (a -> b) -> Parser a -> Parser b
+  pab <*> pa =
+    Parser $
+      (>>= (\(f, x) -> fmap (first f) . runParser pa $ x))
+        . runParser pab
+
+  ---- Parser (a -> b) -> Parser a -> Parser b
+  --pab <*> pa = Parser b
+  --  where
+  --    b str =
+  --      let ab = runParser pab
+  --          a = runParser pa
+  --       in case ab str of
+  --            Just (f, x) -> fmap (first f) (a x)
+  --            Nothing -> Nothing
+    
+
+{- Exercise 3 -}
+
+-- | Maps any parser to a parser that returns an empty tuple (effectively
+-- making it a validator).
+toVoid :: Parser a -> Parser ()
+toVoid = (const () <$>)
+
+-- | Parses character 'a' followed by character 'b'.
+abParser :: Parser (Char, Char)
+abParser = (,) <$> char 'a' <*> char 'b'
+
+-- | The same as @abParser@ but returns an empty tuple instead of @('a', 'b')@.
+abParser_ :: Parser ()
+abParser_ = toVoid abParser
+
+intPair :: Parser [Integer]
+intPair = (\a _ b -> [a,b]) <$> posInt <*> char ' ' <*> posInt
+
+{- Exercise 4 -}
+instance Alternative Parser where
+
+  -- | Return a Parser that always fails (and consumes no input).
+  empty = Parser (const Nothing)
+  
+  -- | @pl <|> pr@ try parsing with @pl@. If this succeeds, return its result.
+  -- Otherwise, return the result of parsing with @pr@.
+  pl <|> pr = Parser $ \x -> runParser pl x <|> runParser pr x
+
+{- Exercise 5 -}
+intOrUppercase :: Parser ()
+intOrUppercase = toVoid posInt <|> toVoid (satisfy isUpper)

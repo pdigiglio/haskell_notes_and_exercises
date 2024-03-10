5cab1a4eb36c88c5ec19bcc6663805fa8d33bfd8
Paolo Di Giglio <p.digiglio91@gmail.com>
2024-03-10 11:50:39 +0100

    week11: add my solutions

--- week11/SExpr.hs
+++ week11/SExpr.hs
@@ -6,26 +6,34 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
+-- | @zeroOrMore p@ tries to parse one or more @p@. If that fails, returns an
+-- empty-list parser.
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure []
 
+-- | @oneOrMore p@ tries to parse once with @p@ and then tries to parse zero or
+-- more times with @p@.
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = (:) <$> p <*> zeroOrMore p
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
+-- | Parse zero or more consecutive spaces.
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore $ satisfy isSpace
 
+-- | Parse an /identifier/, i.e. an alphabetic char followed by zero or more
+-- alphanumeric chars.
 ident :: Parser String
-ident = undefined
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -44,3 +52,22 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+-- | Parse an @Atom@. 
+--
+-- Please note that
+--
+-- > ghci> runParser parseAtom "2fda"
+-- > Just (N 2,"fda")
+--
+-- Instead of returning @Nothing@ (as "2fda" is an invalid identifier).
+parseAtom :: Parser Atom
+parseAtom = N <$> posInt <|> I <$> ident
+
+-- | Parse an @SExpr@ (i.e. an S-expression).
+parseSExpr :: Parser SExpr
+parseSExpr = spaces *> parseTrimmedSExpr <* spaces
+  where
+    parseTrimmedSExpr = toAtom <|> toSExpr
+    toAtom = A <$> parseAtom
+    toSExpr = char '(' *> (Comb <$> oneOrMore parseSExpr) <* char ')'

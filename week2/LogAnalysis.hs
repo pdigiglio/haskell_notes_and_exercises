e915c98b1d60093004131d8123f5722e00040a78
Paolo Di Giglio <p.digiglio91@gmail.com>
2024-04-21 21:04:22 +0200

    week2: improve documentation; remove unused code

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -9,6 +9,24 @@ import Log
 -- (I|W) <timestamp> <content>
 -- E <severity> <timestamp> <content>
 
+
+-- | Get the timestamp out of a log message.
+timeStamp :: LogMessage -> Int
+timeStamp msg = case msg of
+  (LogMessage _ ts _) -> ts
+  _ -> -1
+
+logMsg :: LogMessage -> String
+logMsg (Unknown message) = message
+logMsg (LogMessage _ _ message) = message
+
+{- Exercise 1 -}
+
+-- | Parse and individual line from the log file.
+--
+-- /Note:/ This function assumes that lines starting with "I", "W", or "E" are
+-- in the right format. I.e. "I am not an int" will result in an exception.
+-- Everything else is considered @Unknown@:
 parseMessage :: String -> LogMessage
 parseMessage line = case tokens of
   ("I" : ts : msg) -> LogMessage Info (read ts) (unwords msg)
@@ -18,15 +36,14 @@ parseMessage line = case tokens of
   where
     tokens = words line
 
+-- | Parse a whole log file. 
 parse :: String -> [LogMessage]
-parse = map (parseMessage) . lines
+parse = map parseMessage . lines
 
-timeStamp :: LogMessage -> Int
-timeStamp logMessage = case logMessage of
-  (LogMessage (Error _) ts _) -> ts
-  (LogMessage _ ts _) -> ts
-  _ -> -1
+{- Exercise 2 -}
 
+
+-- | Insert a @LogMessage@ into a (sorted) @MessageTree@.
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
 insert msg Leaf = Node Leaf msg Leaf
@@ -34,56 +51,30 @@ insert msg (Node l m r)
   | timeStamp msg < timeStamp m = Node (insert msg l) m r
   | otherwise = Node l m (insert msg r)
 
+{- Exercise 3 -}
+
+-- | Build a sorted @MessageTree@ from a list of @LogMessages@.
 build :: [LogMessage] -> MessageTree
 build = foldl (\tree msg -> insert msg tree) Leaf
 
+{- Exercise 4 -}
+
+-- | Perform an /in-order/ traversal of a @MessageTree@ and produce a list of
+-- @LogMessage@'s sorted by timestamp.
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf = []
 inOrder (Node l m r) = inOrder l ++ [m] ++ inOrder r
 
-getLogContent :: LogMessage -> String
-getLogContent (Unknown message) = message
-getLogContent (LogMessage _ _ message) = message
+{- Exercise 5 -}
 
-sed :: (LogMessage -> Bool) -> String -> [LogMessage]
-sed f content = filter f $ parse content
-
-sort :: [LogMessage] -> [LogMessage]
-sort = inOrder . build
-
-isError :: LogMessage -> Bool
-isError (LogMessage (Error _) _ _) = True
-isError _ = False
-
-severity :: LogMessage -> Int
-severity (LogMessage (Error s) _ _) = s
-severity _ = -1
-
-isWarn :: LogMessage -> Bool
-isWarn (LogMessage Warning _ _) = True
-isWarn _ = False
-
-isInfo :: LogMessage -> Bool
-isInfo (LogMessage Info _ _) = True
-isInfo _ = False
-
-isUnknown :: LogMessage -> Bool
-isUnknown (Unknown _) = True
-isUnknown _ = False
-
-filterFile inFile f outFile = do
-  txt <- readFile inFile
-  let txt' = foldl (\x y -> x ++ "\n" ++ y) "" $ f $ parse txt
-  writeFile outFile txt'
-
+-- | Take an unsorted @[LogMessage]@ and return a list of error messages with a
+-- severity @(>= 50)@, sorted by timestamp.
 whatWentWrong :: [LogMessage] -> [String]
 whatWentWrong =
-  map getLogContent
-    . take 50
-    . inOrder
-    . build
-    . filter filterMsg
-
-filterMsg :: LogMessage -> Bool
-filterMsg (LogMessage (Error s) _ _) = s >= 50
-filterMsg _ = False
+  map logMsg
+    . sortMessages
+    . filter isSevereMsg
+  where
+    isSevereMsg (LogMessage (Error s) _ _) = s >= 50
+    isSevereMsg _ = False
+    sortMessages = inOrder . build

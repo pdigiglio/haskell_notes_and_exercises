2e99df6c0a88991a93a9a2dfe74da483c44a0079
Paolo Di Giglio <p.digiglio91@gmail.com>
2024-04-25 22:42:05 +0200

    week3: remove unused code

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -3,65 +3,7 @@
 module Golf where
 
 import Data.List
---import Data.List.Split (chunksOf)
 
---main :: IO()
---main = do
---    print $ skips_ "0123456789"
---    print $ skips_ "ABCD"
---    print $ skips_ "Hello!"
---    print $ skips_ [1]
---    print $ skips_ [True, False]
---    --print $ skips []
---    --print $ test 2  "0123456789"
-
-chunk :: Int -> [a] -> [[a]]
-chunk _ []   = []
-chunk n l 
-    | n <= 0    = chunk 0 l
-    | otherwise = ((take n l) : (chunk n $ drop n l))
-
---chunk :: Int -> [a] -> [[a]]
---skips n list =
---
---    $ zip s list
---    where s = map (`div` n) [0..]
---    foldr (:) []
---    . map snd
---    . filter (\(i, _) -> 0 == mod i 2)
---    . zip [0..]
-
---takeEvery :: Int -> [a] -> [a]
---takeEvery 0 l = l
---takeEvery n l =
---    foldr (:) []
---    $ map snd
---    $ filter (\(i, _) -> 0 == mod i n)
---    $ zip [0..] l
---
---skips :: [a] -> [[a]]
---skips i = map (\n -> takeEvery n i) [1 .. length i]
-
---skips :: [a] -> [[a]]
---skips l = map (\n -> takeEvery n i) [1 .. length i]
-
--- ATTEMPT #0 (on Compiler Explorer, which doesn't have `chunk`).
---
--- | 'everyNth' @k l@ selects every element from @l@ whose position is a
--- multiple of @k@.
--- If @k@ is greater than the number of elements in @l@, return an empty list.
--- If @k@ is smaller or equal to 0, return the whole list.
-everyNth :: Int -> [a] -> [a]
-everyNth k l
-    | k <= 0 = l
-    | True = map (head . snd) $ zipWith splitAt [0, k..n] (repeat l)
-    where n = length l - 1
-
-skips' :: [a] -> [[a]]
-skips' =
-    filter ((/= 0) . length) -- Filter-out empty lists
-    . zipWith everyNth [1..] 
-    . tails                  -- Evaluate the tails of the list
 
 {- Exercise 1 -}
 
@@ -113,39 +55,6 @@ skips l = [[c | (i, c) <- zip [1 ..] l, mod i n == 0] | n <- [1 .. length l]]
 --         . filter ((== n) . length)
 --         . chunksOf n
 --     r = [1 .. length l]
-    
-
--- ATTEMPT #1 (with `chunk`)
---
--- The `map` maps a seqence of integers, @k@, from 1 to the length of @l@. For
--- each @k@. It does the following:
---
---  * chunk :: Int -> [a] -> [[a]]
---    `chunk k l` will transform @l@ into a list of sub-lists of length @k@
---    (except for the last one, which may be shorter).
---
---  * filter ((==k) . length) :: [[a]] -> [[a]]
---    Removes all the sub-lists whose length is not @k@. I do this to remove
---    the last sub-list when the length of @l@ is not a multiple of @k@.
---
---  * map last :: [[a]] -> [a]
---    Takes the last element of every sub-list, i.e. every other k-th element
---    in @l@. If I didn't do the filter above, I'd get every other k-th element
---    AND the last element. 
---
-skips_ :: [a] -> [[a]]
-skips_ l = map (everyOtherNth_ l) [1 ..  (length l)]
-
-everyOtherNth_ :: [a] -> Int -> [a]
-everyOtherNth_ l n = map last $ filter ((==n) . length) $ chunk n l
-
-
-
-
---skips s = map everyNth [1..strLen] 
---    where
---        strLen = length s - 1
---        eveyNth k = map (head . snd) $ zipWith splitAt [0, k..strLen] (repeat s)
 
 
 {- Exercise 2 -}
@@ -166,6 +75,7 @@ everyOtherNth_ l n = map last $ filter ((==n) . length) $ chunk n l
 --  2. Filter out all the triplets whose center element is not a local maximum.
 localMaxima :: [Integer] -> [Integer]
 localMaxima l = [y | (x, y, z) <- zip3 l (drop 1 l) (drop 2 l), y > x, y > z]
+
 
 {- Exercise 3 -}
 
